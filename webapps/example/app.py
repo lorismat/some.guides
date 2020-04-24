657cc6e6444e465a931cf229d2c859043164c40d
Loris M <loris.mattioni@dataiku.com>
2020-04-24 17:21:21 +0200

    commit

---
+++ webapps/example/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Setup the dev & prod env
+ENV = 'prod'
+
+if ENV == 'dev':
+    app.debug = True
+    # local database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_user@localhost/your_app_name'
+else:
+    app.debug = False
+    # production database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zvhogdaykwdurq:94c9c78fc63645a2290b71bfc815a52a71eacd1ae4fcdaeb6ca68bc4bb45225c@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d9nnjhc9ed1vf3'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# create the DB object
+db = SQLAlchemy(app)
+
+# Select a model
+class Feedback(db.Model):
+    # name of your table and columns, adding a key column
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True)
+    field = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, name, field, rating, comments):
+        self.name = name
+        self.field = field
+        self.rating = rating
+        self.comments = comments
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        field = request.form['field']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        if name == '' or field == '':
+            return render_template('index.html', message='Please enter required fields')
+        else:
+            data = Feedback(name, field, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('success.html')
+
+if __name__ == '__main__':
+  app.run()
